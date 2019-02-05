eb0ef9d5188efa9a73f8d9f756e5f13f42d40dc2
Aj Carvajal <alanjcarv@gmail.com>
2019-02-04 20:37:24 -0900

    forgot to save

--- backend/api/session.php
+++ backend/api/session.php
@@ -1,8 +1,9 @@
 <?php
 
-class Session extends APIbase{
+class Session {
 
     private $dbConnection;
+    private $authState;
 
     public $id;
     public $date;
@@ -15,35 +16,61 @@ class Session extends APIbase{
 
     public function __construct($db){
         $this->dbConnection = $db;
+        $this->authState = new UserState($this);
     }
 
+    function changeState(AuthState $newState) {
+        $this->authState = $newState;
+    }
 
     function create(){
-        //sql stuff
-        return;
-    }
-
-    function get(){
-        //sql stuff
-        return;
-    }
-
-    function post() {
-        //sql stuff
-        return;
-    }
-
-    function update() {
-        //sql stuff
-        return;
-    }
-
-    function delete() {
-        //sql stuff
-        return;
+        $this->authState->create();
     }
 }
 
+abstract class AuthState {
 
+    protected $session;
+
+    protected function __construct(Session $session) {
+        $this->session = $session;
+    }
+    abstract public function login(): void;
+    abstract public function logout(): void;
+    abstract public function create(): void;
+    abstract public function read(): void;
+    abstract public function update(): void;
+    abstract public function delete(): void;
+}
+
+class UserState extends AuthState {
+
+    function login() {
+        //if successful
+        $this->session->changeState(new AdminState());
+    }
+
+    function logout() {
+        return;
+    }
+
+    function create() {
+        //echo not allowed
+        //return 400 error
+    }
+}
+
+class AdminState extends AuthState {
+    
+    function login() {
+        return;
+    }
+    function logout() {
+        $this->session->changeState(new UserState());
+    }
+    function create() {
+        //do sql stuff
+    }
+}
 
 ?>
