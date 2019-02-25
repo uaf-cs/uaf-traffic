2ea480a7e4af4c98a5ed26b9097ff5737e95bbc7
Chris Bailey <chris@chrisbailey.io>
2019-02-25 13:06:43 -0900

    Basic UI skeleton

---
+++ uaftraffic/uaftraffic/UAFBigFancyButton.swift
@@ -0,0 +1,35 @@
+//
+//  UAFBigFancyButton.swift
+//  uaftraffic
+//
+//  Created by Christopher Bailey on 2/24/19.
+//  Copyright © 2019 University of Alaska Fairbanks. All rights reserved.
+//
+
+import UIKit
+
+class UAFBigFancyButton: UIButton {
+    let normalBackgroundColor = UIColor(red:0.90, green:0.90, blue:0.90, alpha:1.0)
+    let highlightedBackgroundColor = UIColor(red:0.80, green:0.80, blue:0.80, alpha:1.0)
+    
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        backgroundColor = normalBackgroundColor
+        layer.cornerRadius = 3
+    }
+    
+    override open var isHighlighted: Bool {
+        didSet {
+            backgroundColor = isHighlighted ? highlightedBackgroundColor : normalBackgroundColor
+        }
+    }
+    
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
---
+++ uaftraffic/uaftraffic/UAFTrafficCountViewController.swift
@@ -0,0 +1,55 @@
+//
+//  UAFTrafficCountViewController.swift
+//  uaftraffic
+//
+//  Created by Christopher Bailey on 2/24/19.
+//  Copyright © 2019 University of Alaska Fairbanks. All rights reserved.
+//
+
+import UIKit
+
+class UAFTrafficCountViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func endSessionButtonTapped(_ sender: Any) {
+        let confirmation = UIAlertController(title: "End Session", message: "Are you sure you want to end this session?", preferredStyle: .alert)
+        confirmation.addAction(UIAlertAction(title: "Yes, end session", style: .destructive, handler: getSessionName))
+        confirmation.addAction(UIAlertAction(title: "No, continue", style: .cancel, handler: nil))
+        present(confirmation, animated: true, completion: nil)
+    }
+    
+    func getSessionName(sender: UIAlertAction) {
+        let namePrompt = UIAlertController(title: "Session Name", message: "What should this session be called?", preferredStyle: .alert)
+        namePrompt.addTextField { textField in
+            textField.placeholder = "Intersection of main and 3rd"
+        }
+        namePrompt.addAction(UIAlertAction(title: "Save", style: .default, handler: { [weak namePrompt] _ in
+            guard let name = namePrompt!.textFields!.first!.text else { return }
+            self.endSession(name: name)
+        }))
+        namePrompt.addAction(UIAlertAction(title: "Cancel", style: .cancel, handler: nil))
+        present(namePrompt, animated: true, completion: nil)
+    }
+    
+    func endSession(name: String) {
+        print("Ending session")
+        print(name)
+        self.dismiss(animated: true, completion: nil)
+    }
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
