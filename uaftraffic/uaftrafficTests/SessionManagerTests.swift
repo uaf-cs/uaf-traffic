5bae1b5556ec9d1c7e00ad476a6df18b0bb1f2c4
Chris Bailey <chris@chrisbailey.io>
2019-03-03 21:51:51 -0900

    Add SessionManager and Session tests

---
+++ uaftraffic/uaftrafficTests/SessionManagerTests.swift
@@ -0,0 +1,101 @@
+//
+//  SessionManagerTests.swift
+//  uaftrafficTests
+//
+//  Created by Christopher Bailey on 3/3/19.
+//  Copyright © 2019 University of Alaska Fairbanks. All rights reserved.
+//
+
+import XCTest
+@testable import SessionManager
+
+class SessionManagerTests: XCTestCase {
+	var sessionManager: SessionManager!
+	
+    override func setUp() {
+		sessionManager = SessionManager()
+    }
+
+    override func tearDown() {
+		sessionManager = nil
+    }
+
+	// A SessionManager with no sessions associated should return an empty array.
+    func testEmptySessionManager() {
+        XCTAssertEqual(sessionManager.getSessions(), [])
+    }
+
+	// A session, once written, should be readable
+	func testWriteSingleSession() {
+		let session = Session()
+		session.name = "test session"
+		sessionManager.writeSession(session)
+		let sessions = sessionManager.getSessions()
+		XCTAssertEqual(sessions[0], session)
+	}
+
+	// A session, once written, should be readable from a different SessionManager
+	func testWriteSingleSessionMultipleManagers() {
+		// First
+		let session = Session()
+		session.name = "test session"
+		sessionManager.writeSession(session)
+		
+		// Second
+		let altManager = SessionManager()
+		let sessions = altManager.getSessions()
+		XCTAssertEqual(sessions[0], session)
+	}
+	
+	// A session, once written, should be deletable
+	func testDeleteSession() {
+		let session1 = Session()
+		let session2 = Session()
+		sessionManager.writeSession(session1)
+		sessionManager.writeSession(session2)
+		sessionManager.deleteSession(session2)
+		let remainingSessions = sessionManager.getSessions()
+		XCTAssertEqual(remainingSessions.count, 1)
+		XCTAssertEqual(remainingSessions[0], session1)
+	}
+	
+	// A session, once written, should be deletable (alternate)
+	func testDeleteSessionAlt() {
+		let session1 = Session()
+		let session2 = Session()
+		sessionManager.writeSession(session1)
+		sessionManager.writeSession(session2)
+		sessionManager.deleteSession(session1)
+		let remainingSessions = sessionManager.getSessions()
+		XCTAssertEqual(remainingSessions.count, 1)
+		XCTAssertEqual(remainingSessions[0], session2)
+	}
+	
+	// A session, once written, should be deletable from a different manager
+	func testDeleteSessionMultipleManagers() {
+		let session1 = Session()
+		let session2 = Session()
+		sessionManager.writeSession(session1)
+		sessionManager.writeSession(session2)
+		
+		let sessionManager2 = SessionManager()
+		sessionManager2.deleteSession(session2)
+		let remainingSessions = sessionManager2.getSessions()
+		XCTAssertEqual(remainingSessions.count, 1)
+		XCTAssertEqual(remainingSessions[0], session1)
+	}
+	
+	// A session, once written, should be deletable from a different manager (alternate)
+	func testDeleteSessionMultipleManagersAlt() {
+		let session1 = Session()
+		let session2 = Session()
+		sessionManager.writeSession(session1)
+		sessionManager.writeSession(session2)
+		
+		let sessionManager2 = SessionManager()
+		sessionManager2.deleteSession(session1)
+		let remainingSessions = sessionManager2.getSessions()
+		XCTAssertEqual(remainingSessions.count, 1)
+		XCTAssertEqual(remainingSessions[0], session2)
+	}
+}
---
+++ uaftraffic/uaftrafficTests/SessionTests.swift
@@ -0,0 +1,81 @@
+//
+//  SessionTests.swift
+//  uaftrafficTests
+//
+//  Created by Christopher Bailey on 3/3/19.
+//  Copyright © 2019 University of Alaska Fairbanks. All rights reserved.
+//
+
+import XCTest
+@testable import Session
+
+class SessionTests: XCTestCase {
+	let session: Session!
+	
+    override func setUp() {
+        session = Session()
+    }
+
+    override func tearDown() {
+        session = nil
+    }
+	
+	// Sessions IDs should be auto-generated and unique
+	func testSessionIdGeneration() {
+		let session2 = Session()
+		XCTAssertGreaterThan(session.id.count, 0)
+		XCTAssertGreaterThan(session2.id.count, 0)
+		XCTAssertNotEqual(session.id, session2.id)
+	}
+
+	// addCrossing method should work
+	func testAddCrossing() {
+		session.addCrossing("car", "w", "n" )
+		XCTAssertEqual(session.crossings[0].type, "car")
+		XCTAssertEqual(session.crossings[0].from, "w")
+		XCTAssertEqual(session.crossings[0].to, "n")
+		XCTAssertEqual(session.crossings.count, 1)
+	}
+	
+	// addCrossing should work multiple times
+	func testAddTwoCrossings() {
+		
+		// Test first crossing
+		session.addCrossing("car", "w", "n" )
+		XCTAssertEqual(session.crossings[0].type, "car")
+		XCTAssertEqual(session.crossings[0].from, "w")
+		XCTAssertEqual(session.crossings[0].to, "n")
+		XCTAssertEqual(session.crossings.count, 1)
+		
+		// Test second crossing
+		session.addCrossing("pedestrian", "e", "s" )
+		XCTAssertEqual(session.crossings[1].type, "pedestrian")
+		XCTAssertEqual(session.crossings[1].from, "e")
+		XCTAssertEqual(session.crossings[1].to, "s")
+		XCTAssertEqual(session.crossings.count, 2)
+	}
+	
+	// undo should remove the last crossing
+	func testUndo() {
+		// Test first crossing
+		session.addCrossing("car", "w", "n" )
+		XCTAssertEqual(session.crossings[0].type, "car")
+		XCTAssertEqual(session.crossings[0].from, "w")
+		XCTAssertEqual(session.crossings[0].to, "n")
+		XCTAssertEqual(session.crossings.count, 1)
+		
+		// Test second crossing
+		session.addCrossing("pedestrian", "e", "s" )
+		XCTAssertEqual(session.crossings[1].type, "pedestrian")
+		XCTAssertEqual(session.crossings[1].from, "e")
+		XCTAssertEqual(session.crossings[1].to, "s")
+		XCTAssertEqual(session.crossings.count, 2)
+		
+		// Test undo second crossing
+		session.undo()
+		XCTAssertEqual(session.crossings[0].type, "car")
+		XCTAssertEqual(session.crossings[0].from, "w")
+		XCTAssertEqual(session.crossings[0].to, "n")
+		XCTAssertEqual(session.crossings.count, 1)
+	}
+}
