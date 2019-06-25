07391905c23e884fa3216ba4b06f8301a98b63d6
jwolf <jwolf@Jonathans-MacBook-Pro.local>
2019-06-24 16:07:35 -0800

    Added View controller for the direction selection screen

---
+++ uaftraffic/uaftraffic/DirectionSelectViewController.swift
@@ -0,0 +1,9 @@
+//
+//  DirectionSelectController.swift
+//  uaftraffic
+//
+//  Created by jwolf on 6/24/19.
+//  Copyright © 2019 University of Alaska Fairbanks. All rights reserved.
+//
+
+import Foundation
