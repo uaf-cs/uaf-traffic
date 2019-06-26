79da4aeef071420afc6d8431c3f7a2117209fe9b
jwolf <jwolf@Jonathans-MacBook-Pro.local>
2019-06-26 14:55:37 -0800

    corrected the aforementioned fatal error

--- uaftraffic/uaftraffic/DirectionSelectViewController.swift
+++ uaftraffic/uaftraffic/DirectionSelectViewController.swift
@@ -17,4 +17,26 @@ class DirectionSelectViewController: UITableViewController {
     override func numberOfSections(in tableView: UITableView) -> Int {
         return 1
     }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 4;
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
+        let counter = indexPath.row
+        switch counter{
+        case 0:
+            cell.textLabel?.text = "North"
+        case 1:
+            cell.textLabel?.text = "East"
+        case 2:
+            cell.textLabel?.text = "South"
+        case 3:
+            cell.textLabel?.text = "West"
+        default:
+            assert(false, "too many rows")
+        }
+        return cell
+    }
 }
