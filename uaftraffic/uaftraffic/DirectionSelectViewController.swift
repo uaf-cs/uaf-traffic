3b110b3462f1822c4fa9a52b63ab1ef5357ad00e
jwolf <jwolf@Jonathans-MacBook-Pro.local>
2019-06-26 12:18:55 -0800

    reverted previous change to undo a fatal error

--- uaftraffic/uaftraffic/DirectionSelectViewController.swift
+++ uaftraffic/uaftraffic/DirectionSelectViewController.swift
@@ -14,10 +14,6 @@ class DirectionSelectViewController: UITableViewController {
     self.dismiss(animated: true, completion: nil)
     }
     
-    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 4
-    }
-    
     override func numberOfSections(in tableView: UITableView) -> Int {
         return 1
     }
