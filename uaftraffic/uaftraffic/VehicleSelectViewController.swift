99d4e18915b162f6bb72e39efed60eb1214a535a
jwolf <jwolf@Jonathans-MacBook-Pro.local>
2019-06-26 15:53:42 -0800

    Vehicle Selection Screen created, options removed

---
+++ uaftraffic/uaftraffic/VehicleSelectViewController.swift
@@ -0,0 +1,9 @@
+//
+//  VehicleSelectViewController.swift
+//  uaftraffic
+//
+//  Created by jwolf on 6/26/19.
+//  Copyright © 2019 University of Alaska Fairbanks. All rights reserved.
+//
+
+import Foundation
