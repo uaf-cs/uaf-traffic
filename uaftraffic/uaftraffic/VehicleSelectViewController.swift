06b113ea7228da32cfbdc57cbf7b90a76716357a
jwolf <jwolf@Jonathans-MacBook-Pro.local>
2019-06-26 16:45:58 -0800

    Session naming function prepared, but not working

--- uaftraffic/uaftraffic/VehicleSelectViewController.swift
+++ uaftraffic/uaftraffic/VehicleSelectViewController.swift
@@ -18,35 +18,30 @@ class VehicleSelectViewController: UITableViewController {
         return 5;
     }
     
-    /*
-     func getSessionName(sender: UIAlertAction) {
+     @IBAction func getSessionName(sender: Any) {
      let namePrompt = UIAlertController(title: "Session Name", message: "What should this session be called?", preferredStyle: .alert)
      namePrompt.addTextField { textField in
      textField.placeholder = "Intersection of main and 3rd"
      }
      namePrompt.addAction(UIAlertAction(title: "Save", style: .default, handler: { [weak namePrompt] _ in
      guard let name = namePrompt!.textFields!.first!.text else { return }
-     self.endSession(name: name)
+     self.saveSession(name: name)
      }))
-     namePrompt.addAction(UIAlertAction(title: "Quit without saving", style: .destructive, handler: { _ in
-     let confirmation = UIAlertController(title: "Quit without saving", message: "Are you sure you want to quit without saving?", preferredStyle: .alert)
-     confirmation.addAction(UIAlertAction(title: "Yes, discard data", style: .destructive, handler: { _ in
-     self.dismiss(animated: true, completion: nil)
-     }))
-     confirmation.addAction(UIAlertAction(title: "No, continue", style: .cancel, handler: nil))
-     self.present(confirmation, animated: true, completion: nil)
-     }))
-     namePrompt.addAction(UIAlertAction(title: "Cancel", style: .cancel, handler: nil))
+     namePrompt.addAction(UIAlertAction(title: "Cancel", style: .cancel, handler: cancel))
      present(namePrompt, animated: true, completion: nil)
      }
      
-     func endSession(name: String) {
-     print("Ending session")
+     func cancel(sender: UIAlertAction){
+        self.dismiss(animated: true, completion: nil)
+     }
+     
+     func saveSession(name: String) {
+     let session = Session()
      session.name = name
+     let sessionManager = SessionManager()
      sessionManager.writeSession(session: session)
      self.dismiss(animated: true, completion: nil)
      }
-    */
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
