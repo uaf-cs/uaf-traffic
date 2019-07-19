b3d74fe9fd438f596e0ab383b18976a89f234f53
jwolf <jwolf@Jonathans-MacBook-Pro.local>
2019-07-19 10:26:30 -0800

    prototyping begun on the summary page

--- uaftraffic/uaftraffic/TrafficSummaryViewCell.swift
+++ uaftraffic/uaftraffic/TrafficSummaryViewCell.swift
@@ -2,8 +2,13 @@
 //  TrafficSummaryViewCell.swift
 //  uaftraffic
 //
-//  Created by jwolf on 7/19/19.
+//  Created by Joseph Wolf on 7/19/19.
 //  Copyright © 2019 University of Alaska Fairbanks. All rights reserved.
 //
 
 import Foundation
+import UIKit
+
+class TrafficSummaryViewCell: UITableViewCell{
+    
+}
--- uaftraffic/uaftraffic/TrafficSummaryViewController.swift
+++ uaftraffic/uaftraffic/TrafficSummaryViewController.swift
@@ -2,8 +2,17 @@
 //  TrafficSummaryViewController.swift
 //  uaftraffic
 //
-//  Created by jwolf on 7/19/19.
+//  Created by Joseph Wolf on 7/19/19.
 //  Copyright © 2019 University of Alaska Fairbanks. All rights reserved.
 //
 
 import Foundation
+import UIKit
+
+class TrafficSummaryViewController: UITableViewController{
+    
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        return 4
+    }
+    
+}
