db35f4ce7f8e707ba5c6b25e0a40bba705928319
jwolf <jwolf@Jonathans-MacBook-Pro.local>
2019-07-19 10:25:41 -0800

    Prototyping begun for the summary page

---
+++ uaftraffic/uaftraffic/TrafficSummaryViewCell.swift
@@ -0,0 +1,9 @@
+//
+//  TrafficSummaryViewCell.swift
+//  uaftraffic
+//
+//  Created by jwolf on 7/19/19.
+//  Copyright © 2019 University of Alaska Fairbanks. All rights reserved.
+//
+
+import Foundation
---
+++ uaftraffic/uaftraffic/TrafficSummaryViewController.swift
@@ -0,0 +1,9 @@
+//
+//  TrafficSummaryViewController.swift
+//  uaftraffic
+//
+//  Created by jwolf on 7/19/19.
+//  Copyright © 2019 University of Alaska Fairbanks. All rights reserved.
+//
+
+import Foundation
