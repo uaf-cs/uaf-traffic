2c51cbd4dc57480c3f2bf272360a239353513b3e
jwolf <jwolf@Jonathans-MacBook-Pro.local>
2019-08-06 15:46:11 -0800

    sketch part 2

--- uaftraffic/uaftraffic/Formatters.swift
+++ uaftraffic/uaftraffic/Formatters.swift
@@ -2,8 +2,22 @@
 //  Formatters.swift
 //  uaftraffic
 //
-//  Created by jwolf on 8/6/19.
+//  Created by Joseph Wolf on 8/6/19.
 //  Copyright © 2019 University of Alaska Fairbanks. All rights reserved.
 //
 
 import Foundation
+
+/*
+// Pieces to be uncommented as they become developed with proper code
+ 
+ Lat/Long Formatter(s){
+ Specify no more than two digits on either side of a decimal place, and either N/S or E/W (depending on whether lat or lon is being checked)
+ }
+ 
+ Street Formatter{
+ No punctuation, possibly check for name and street type, but with name potentially being more than 1 word, may prove difficult to implement
+ }
+ 
+ //These shall be custom formatters, which inherit from the abstract class formatter.
+ */
