0d8d75cb3242e16f450abda0c8895e973cd81bc4
jwolf <jwolf@Jonathans-MacBook-Pro.local>
2019-08-08 09:39:29 -0800

    Form data now saved to a session

--- uaftraffic/uaftraffic/SessionInfoViewController.swift
+++ uaftraffic/uaftraffic/SessionInfoViewController.swift
@@ -16,7 +16,27 @@ class SessionInfoViewController: UIViewController{
     @IBOutlet weak var ewField: UITextField!
     @IBOutlet weak var nsField: UITextField!
     @IBOutlet weak var technicianField: UITextField!
+    var session = Session()
 
+    @IBAction func saveInfo(_ sender: Any){
+        let name = nameField.text!
+        self.session.name = name
+        let lat = latField.text!
+        self.session.lat = lat
+        //        self.session.lat = (testFormatter.number(from: lat)?.stringValue ?? "00.00") + " N"
+        let lon = lonField.text!
+        self.session.lon = lon
+        //        self.session.lon = (testFormatter.number(from: lon)?.stringValue ?? "00.00") + " W"
+        let ewRoad = ewField.text!
+        self.session.EWRoadName = ewRoad
+        let nsRoad = nsField.text!
+        self.session.NSRoadName = nsRoad
+        let userName = technicianField.text!
+        self.session.technician = userName
+        
+        let sessionManager = SessionManager()
+        sessionManager.writeSession(session: session)
+    }
     
 }
 
